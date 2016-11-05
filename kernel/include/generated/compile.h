31dc2312cd8be5527fbee8454bf8d0d4e781ee19
kirito96 <eagle1968@live.com>
2016-11-04 22:43:51 -0400

    first commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Fri Nov 4 22:08:12 EDT 2016"
+#define LINUX_COMPILE_BY "kirito9"
+#define LINUX_COMPILE_HOST "kirito9"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
